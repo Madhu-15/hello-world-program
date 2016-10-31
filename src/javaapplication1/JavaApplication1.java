435be35d8d5932c0549f3a71b8793ad881b9a40b
Admin <Admin@Admin-PC.mshome.net>
2016-10-31 08:32:00 +0530

    java

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -4,6 +4,9 @@
  */
 package javaapplication1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Admin
@@ -14,6 +17,30 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-   System.out.println("Hello");
+        ArrayList<Integer> numbers = new ArrayList<>();
+ // Adding
+        numbers.add(10);
+        numbers.add(100);
+        numbers.add(40);
+ // Retrieving
+        System.out.println(numbers.get(0));
+  System.out.println("nIteration #1: ");
+        // Indexed for loop iteration
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.println(numbers.get(i));
+           // Removing items (careful!)
+            Integer remove = numbers.remove(numbers.size() - 1);
+            // This is VERY slow
+        numbers.remove(0);
+ 
+        System.out.println("nIteration #2: ");
+        for (Integer value : numbers) {
+            System.out.println(value);
+                   // List interface ...
+        List<String> values = new ArrayList<String>();
+ 
+        }
+        }
+ 
     }
 }
